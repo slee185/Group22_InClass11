8ea389e743bedd0c56d167b7410f66064746191a
Ken Stanley <dohpaz@gmail.com>
2022-10-31 22:53:57 -0400

    Guard against negative numbers

--- app/src/main/java/edu/uncc/inclass11/AddCourseFragment.java
+++ app/src/main/java/edu/uncc/inclass11/AddCourseFragment.java
@@ -43,11 +43,17 @@ public class AddCourseFragment extends Fragment {
             int selectedId = binding.radioGroupGrades.getCheckedRadioButtonId();
 
             if(courseName.isEmpty() || courseNumber.isEmpty() || hoursString.isEmpty()) {
-               Toast.makeText(getContext(), "Please enter all the fields", Toast.LENGTH_SHORT).show();
-            } else if(selectedId == -1){
+                Toast.makeText(getContext(), "Please enter all the fields", Toast.LENGTH_SHORT).show();
+            } else if (selectedId == -1){
                 Toast.makeText(getContext(), "Please select a letter grade !!", Toast.LENGTH_SHORT).show();
             } else {
                 double courseHours = Double.parseDouble(hoursString);
+
+                if (courseHours < 0) {
+                    Toast.makeText(requireContext(), "Please enter a positive number for hours", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 String courseLetterGrade;
                 if(selectedId == R.id.radioButtonA) {
                     courseLetterGrade = "A";
