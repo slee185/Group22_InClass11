44a14a8a32ac3d9875674db65bdaec24dba6a8e3
Stephanie <slee185@uncc.edu>
2022-11-01 19:17:06 -0400

    attempt so far for part 3

--- app/src/main/java/edu/uncc/inclass11/AddCourseFragment.java
+++ app/src/main/java/edu/uncc/inclass11/AddCourseFragment.java
@@ -22,8 +22,7 @@ public class AddCourseFragment extends Fragment {
 
     FragmentAddCourseBinding binding;
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentAddCourseBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
